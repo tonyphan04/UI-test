eb900a6ea766e52776c9ec9a7a926af505b92743
Hoang Anh Phan <hoanganhphan2k@gmail.com>
2022-08-08 23:41:01 +1000

    Update log in page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Carousel from './Carousel'
+import LogIn from './LogIn'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Carousel />
+    <App></App>
+    <LogIn></LogIn>
   </React.StrictMode>
 );
 
