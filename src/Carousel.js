3b335d5b85f55ef1549d648266e0a6acd96f530d
Hoang Anh Phan <hoanganhphan2k@gmail.com>
2022-08-08 11:57:20 +1000

    First commit

---
+++ src/Carousel.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/swiper.min.css'
+
+import './style.css'
+
+import ts_1 from './media/download.jpg'
+import ts_2 from './media/download1.jpg'
+import ts_3 from './media/download2.jpg'
+
+const images = [ts_1, ts_2, ts_3]
+
+export default () => {
+    return (
+        <div className='carousel'>
+            <h2>Carousel</h2>
+            <Swiper
+                spaceBetween={10}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={image} height='500' className='carousel-image'></img>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
